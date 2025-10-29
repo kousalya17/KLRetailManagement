53bf5837a37f2941925fe816d8c5ba0fca02413e
kousalyal1703 <kousalyal1703@gmail.com>
2025-10-29 21:59:10 +0530

    Added WPF,front end project

---
+++ KRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using KRMDesktopUI.ViewModels;
+using System.Windows;
+
+namespace KRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
