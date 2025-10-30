df39693466cf06b33d4189d83ef2f902dcc17087
kousalyal1703 <kousalyal1703@gmail.com>
2025-10-30 18:26:23 +0530

    Included Container DI system and Tables to the database.

--- KRMDesktopUI/Bootstrapper.cs
+++ KRMDesktopUI/Bootstrapper.cs
@@ -1,19 +1,55 @@
 ﻿using Caliburn.Micro;
 using KRMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace KRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container.Singleton<IWindowManager, WindowManager>()
+                       .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(Type => Type.IsClass)
+                .Where(Type => Type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType)) ;
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
         }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
     }
 }
+
+  
